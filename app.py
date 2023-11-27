fe37c2bb1ffe6fba6da1f9b578cdaef11aaaceaa
Felipe Valencia <afvalenciacas@hotmail.com>
2023-11-26 22:52:14 -0500

    Actualización API

--- app.py
+++ app.py
@@ -1,14 +1,82 @@
 from flask import Flask, render_template, request, jsonify, g
 from flask_cors import CORS
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+from scipy.sparse import csr_matrix
+
+
+df = pd.read_csv("df_spotifynew.csv", sep='|')
+df1 = df.copy()
+
+def custom_recommendation_model(df, generos_usuario, seleccion_usuario, n_components, scaling_method, top_n):
+    try:
+
+        subset_df = df[(df['genero_principal'].isin(generos_usuario)) & (df['sentimiento'] == seleccion_usuario)]
+        if subset_df.shape[0] == 0:
+            subset_df = df[(df['genero_principal'].isin(generos_usuario)) | (df['sentimiento'] == seleccion_usuario)]
+            if subset_df.shape[0] == 0:
+                return pd.DataFrame()
+                
+        atributos_deseados = ['valence', 'year', 'acousticness', 'danceability', 'energy', 'explicit',
+                             'instrumentalness', 'key', 'liveness', 'loudness', 'mode', 'speechiness', 'tempo']
+
+        atributos = subset_df[atributos_deseados].values
+
+        # Aplicar el escalado
+        if scaling_method == "StandardScaler":
+            scaler = StandardScaler()
+            atributos = scaler.fit_transform(atributos)
+        elif scaling_method == "MinMaxScaler":
+            scaler = MinMaxScaler()
+            atributos = scaler.fit_transform(atributos)
+        elif scaling_method == "RobustScaler":
+            scaler = RobustScaler()
+            atributos = scaler.fit_transform(atributos)
+
+        # Reducción de dimensionalidad (SVD)
+        svd = TruncatedSVD(n_components=n_components)
+        atributos_latentes = svd.fit_transform(atributos)
+
+        # Convertir a matriz dispersa
+        atributos_latentes_sparse = csr_matrix(atributos_latentes)
+
+        # Calcular similitud del coseno
+        print(f'Recomendaciones Modelo: antes de la similitud')
+        similitud = cosine_similarity(atributos_latentes_sparse) if n_components < min(atributos.shape) else cosine_similarity(atributos)
+
+        print(f'Recomendaciones Modelo: despues de la similitud')  
+        indices_recomendaciones = similitud.sum(axis=0).argsort()[::-1]
+
+        # Verificar si hay al menos una recomendación
+        if len(indices_recomendaciones) > 0:
+            # Obtener las recomendaciones
+            recomendaciones = subset_df.iloc[indices_recomendaciones].head(10)
+            print(f'Recomendaciones Modelo: {recomendaciones.head(10)}')
+        else:
+            print('No hay canciones para recomendar.')
+            recomendaciones = pd.DataFrame()
+
+    except Exception as e:
+        print(f'Error durante el proceso de recomendación: {e}')
+
+    return recomendaciones
 
 
 app = Flask(__name__)
 CORS(app)  # Esto permite solicitudes CORS desde cualquier origen
 
+app.config['g_sentimiento'] = ""
+app.config['g_generos'] = []
+app.config['g_canciones_no_gustadas'] = []
+
 def before_request():
-    g.sentimiento = ""
-    g.generos = ""
-    g.contador = 0
+    app.config['g_sentimiento'] = ""
+    app.config['g_generos'] = []
+    app.config['g_canciones_no_gustadas'] = []
 
 @app.route('/')
 def index():
@@ -16,11 +84,11 @@ def index():
 
 @app.route('/encuesta')
 def encuesta():
-    return render_template('encuesta.html')
+    return render_template('encuesta.html', g_sentimiento=app.config['g_sentimiento'])
 
 @app.route('/genero')
 def genero():
-    return render_template('genero.html')
+    return render_template('genero.html', g_generos=app.config['g_generos'])
 
 @app.route('/playlist')
 def playlist():
@@ -36,15 +104,17 @@ def recibir_sentimiento():
         data = request.get_json()
         sentimiento = data.get('sentimiento')
 
+        app.config['g_sentimiento'] = sentimiento
+
         # Imprimir el sentimiento en la consola del servidor Flask
         print(f'Sentimiento recibido: {sentimiento}')
 
         # Mensaje a mostrar en el navegador
         mensaje_navegador = f'El sentimiento elegido por el usuario es {sentimiento}'
-        g.sentimiento = sentimiento
+
         # Aquí puedes realizar acciones con el sentimiento recibido
         # En este ejemplo, simplemente lo devolvemos como parte de la respuesta
-        return jsonify({'mensaje': mensaje_navegador}), 200
+        return jsonify({'mensaje': f'Sentimiento: {sentimiento}'}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -54,7 +124,8 @@ def recibir_generos():
     try:
         data = request.get_json()
         generos = data.get('generos')
-        g.generos =  generos
+        app.config['g_generos'] = generos
+        
         # Imprimir los géneros en la consola del servidor Flask
         print(f'Géneros recibidos: {generos}')
 
@@ -70,30 +141,50 @@ def recibir_generos():
 def generar_playlist():
     try:
         data = request.get_json()
-        contador = data.get('contador')
+        # Aqui se hace la llamada del modelo que devuelve una play list segun sentimiento, generos
+             
+        print(f"generar Play List")   
+        sentimiento = app.config['g_sentimiento']
+        generos = app.config['g_generos']
 
-        # Aqui se hace la llamada del modelo que devuelve una play list segun sentimiento, generos y el contador
-        playlist = ""
+        print(f"Sentimiento: {sentimiento}")
+        print(f"Géneros: {generos}")
 
-        if contador == 1:
-            playlist = "<ul><li>nombre_cancion_1 - Almara</li><li>nombre_cancion_2 - Alejandro Sanz</li></ul>"
-        elif contador == 2:
-            playlist = "<ul><li>nombre_cancion_2 - Skakira </li><li>nombre_cancion_2 - Maluma</li></ul>"
-        else: 
-            playlist = ""
-        # si la playlist esta vacia en la respuesta de javascript sabemos que hay volver a la pagina principal
+        # Verificar si hay canciones no gustadas antes de aplicar el filtrado
+        v_canciones_no_gustadas = app.config['g_canciones_no_gustadas']
+        print(f"v_canciones_no_gustadas: {v_canciones_no_gustadas}")
+        print(f"len v_canciones_no_gustadas: {len(v_canciones_no_gustadas)}")    
 
-        # Mensaje a mostrar en el navegador
-        mensaje_navegador = f'La Playlist es {playlist}'
+        if len(v_canciones_no_gustadas) == 0:
+            df1 = df.copy()
+            print(f" df1 = df.copy()")
+        else:
+            df1 = df[~df['id'].isin(v_canciones_no_gustadas)]
+            print(f" Elimino Canciones no Gustadas")
+ 
+        #playlist = "<ul><li>nombre_cancion_1 - Almara</li><li>nombre_cancion_2 - Alejandro Sanz</li></ul>"
+        
+        print(f" .... Generando Recomendaciones .... ")
+        # Obtner Recomendaciones
+        df_recomendaciones = custom_recommendation_model(df1, generos, sentimiento, n_components= 5 , scaling_method = "RobustScaler" , top_n = 10)
+        print(f" .... Generando Recomendaciones Completado .... ")
+        
+        if df_recomendaciones.shape[0] == 0:
+             playlist = "Desafortunadamente, la biblioteca no cuenta con canciones disponibles en este momento"
+        else:
+            df_playlist = df_recomendaciones[["name", "artists"]]
+            playlist = "\n".join(df_playlist.apply(lambda row: ' - '.join(row), axis=1))
 
-        print(f'Playlist recibidos: {playlist}')
-
-        # Aquí puedes realizar acciones con el sentimiento recibido
-        # En este ejemplo, simplemente lo devolvemos como parte de la respuesta
+            # remover las recomendaciones en caso que al usuario no le gusten
+            canciones_no_gustadas = df_recomendaciones['id'].tolist()
+            app.config['g_canciones_no_gustadas'].extend(canciones_no_gustadas)
+            #print(f'Canciones no Gustadas: {canciones_no_gustadas}')
+        
+        print(f'Playlist: {playlist}')
         return jsonify({'playlist': f'{playlist}'}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
